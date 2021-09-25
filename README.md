f2125d9faa7ab8f1b84e2888333211055e54086b
Shin Yi <contact.shinyi@gmail.com>
2021-09-25 13:32:19 +0800

    randomised grid and removed jquery

--- README.md
+++
@@ -1,9 +0,0 @@
-# Minesweeper Rat 🐀
-A single player game where the objective is to the clear the board without denotating the hidden landmines. You may play a heroRat on the tile that is suspected to contain a hidden landmine.
-
-## How to play
-Left click on the tiles in the gameboard. The numbers will reveal how many landmines surrounds that tile.
-Right click on the tiles that are suspected to contain a hidden landmine. A heroRat will check the tile for any landmines, and reminding the player not to click on the tile anymore. Right click on the tile again to remove the heroRat.
-
-## Features
--	
--- app.js
+++ app.js
@@ -1,28 +1,33 @@
-$(()=>{  
-    minefield(100);
+let gameboard = [];
+let numBombs = 20; //number of bombs
 
-    $('.tile').mousedown(function(event){
-        if(event.which == 3)
-        {
-              alert("right mouse click");
-              addMouse();
+//creates the gameboard
+let minefield = (grids) => {
+    //randomise bombs and void tiles. attach as a class to each grid.
+    const bombtiles = Array.from({length: numBombs}, () => 'bomb'); //makes an array of 'bombtile', which are grids with bombs
+    const voidtiles = Array.from({length: grids-numBombs}, () => 'void'); //makes an array of 'voidtile', which are grids without bombs
+    const alltiles = [...bombtiles, ...voidtiles]; //makes a new array by combining bomb and void tiles
+    
+    //function to randomise array elememts, Fisher-Yates algorithm
+    const shuffleArray = array => {
+        for (let i = array.length - 1; i > 0; i--) {
+          const j = Math.floor(Math.random() * (i + 1));
+          const temp = array[i];
+          array[i] = array[j];
+          array[j] = temp;
         }
-        if(event.which == 1)
-        {
-              alert("left mouse click");
-        }
-    });
-})
+        return array;
+      }
+    const randomtiles = shuffleArray(alltiles); //randomise the elements in the array
 
-
-
-
-
-//add game board
-let minefield = (num) => {
-    for (let i=0;i<num;i++) {
-        const $tile=$('<div>').addClass('tile').attr('id', `tile-${i}`);
-        $('.container').append($tile);
+    //generate x number of grids on the gameboard
+    for (let i=0;i<grids;i++) {
+        const container = document.querySelector('.container');
+        const newtile = document.createElement('div');
+        newtile.id = `tile-${i}`; 
+        newtile.classList.add(`${randomtiles[i]}`); 
+        container.appendChild(newtile);
+        gameboard.push(newtile);
     }
 }
 
@@ -30,3 +35,29 @@ let minefield = (num) => {
 let addMouse = () => {
     $('.tile').html('🐀');
 }
+
+//remove flag
+let lessMouse = () => {
+    $('.tile').html('');
+}
+
+document.addEventListener('DOMContentLoaded', () => {  
+    minefield(100);
+
+    // $('.tile').mousedown(function(event){
+    //     if(event.which == 3)
+    //     {
+    //         // if (".tile:contains('🐀')") {
+    //         //     alert("right mouse click");
+    //         //     lessMouse();
+    //         //  } else {
+    //             alert("r1ight mouse click");
+    //             addMouse();
+    //         //  };     
+    //     };
+    //     if(event.which == 1)
+    //     {
+    //           alert("left mouse click");
+    //     };
+    // });
+});
